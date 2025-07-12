d80d128ccbf3d6d3f1ee12005e581aab02c2cc9f
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-07-12 13:03:51 +0530

    Update mergeTwoSortedArray.js

--- Array/mergeTwoSortedArray.js
+++ Array/mergeTwoSortedArray.js
@@ -1,3 +1,29 @@
+
+// TC -- O(m+n)
+// SC -- O(1)
+var merge = function(nums1, m, nums2, n) {
+    // start from the last indexes
+    let i = m-1;
+    let j= n-1;
+    let k = m+n-1;
+    while (j >= 0) {
+        if( i >=0 && nums1[i] > nums2[j]){
+            nums1[k] = nums1[i]
+            k--
+            i--
+
+        }
+        else {
+            nums1[k] = nums2[j]
+            k--
+            j--
+
+
+        }
+    }
+    
+};
+
 /**
  * @param {number[]} nums1
  * @param {number} m
