ed704fa272965791f98bf61911b57d4910f6c7c6
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-07-15 13:48:55 +0530

    Update missingNumber.js

--- Array/missingNumber.js
+++ Array/missingNumber.js
@@ -1,6 +1,25 @@
 // TC --- O(n)
 // SC -- O(1)
 var missingNumber = function(nums) {
+    let n = nums.length;
+    let xor = 0
+    // XOR all array elements
+    for(let i=0; i<n; i++){
+        xor = xor ^ nums[i]
+    }
+    //XOR all expected values from 0 to n
+    for(let i=0; i<=n; i++){
+        xor = xor ^ i
+
+    }
+   
+    return xor
+    
+};
+//-----------------------------------------------------------------------------------------------------------------
+// TC --- O(n)
+// SC -- O(1)
+var missingNumber = function(nums) {
     const n = nums.length
     // formula to find the sum of number [1 - n]
     const totalSum = n * (n + 1) / 2
