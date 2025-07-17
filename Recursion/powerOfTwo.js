2631e34a3aff40f54daab42c9142480ae3a78e54
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-07-17 14:16:54 +0530

    Update powerOfTwo.js

--- Recursion/powerOfTwo.js
+++ Recursion/powerOfTwo.js
@@ -6,6 +6,6 @@ var isPowerOfTwo = function(n) {
     if(n===1) return true
     // odd number can't be power of 2 except 1
     if(n %2 !==0 ) return false
-    // keep dividing the number until turns 1
-    return isPowerOfTwo(Math.floor(n/2))
+    // keep dividing the number 
+    return isPowerOfTwo(n/2)
 };
