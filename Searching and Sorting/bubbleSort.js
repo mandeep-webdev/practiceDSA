19aedf219a1f333b6fec0ad5b75f657422dad662
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-07-19 13:58:02 +0530

    Update bubbleSort.js

--- Searching and Sorting/bubbleSort.js
+++ Searching and Sorting/bubbleSort.js
@@ -1,14 +1,21 @@
 // TC-- O(n*n) SC -- O(1)
+
 var sortArray = function(nums) {
     const n = nums.length
-    for(let i=0; i<n; i++){
-        for(let j=0; j<n-i; j++){
+    
+    for(let i=0; i<n-1; i++){
+        let isSwapped = false
+        for(let j=0; j<n-1-i; j++){
             if(nums[j] > nums[j+1]){
                 // swap
+                
                 [nums[j],nums[j+1]] = [nums[j+1],nums[j]]
+                isSwapped = true
             }
 
         }
+        if(!isSwapped) return nums
+       
 
     }
     return nums
