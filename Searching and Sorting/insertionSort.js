b241d90256234e6ef5ae65e75b12d8d6ca463b1c
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-07-22 13:38:37 +0530

    Create insertionSort.js

---
+++ Searching and Sorting/insertionSort.js
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/problems/insertion-sort/1
+// TC -- O(n) // SC -- O(1)
+class Solution {
+    insertionSort(arr) {
+        // code here
+        let n = arr.length
+        for(let i=1; i<n; i++){
+            let cur = arr[i]
+            let prev = i-1
+            while(arr[prev] > cur && prev >=0){
+                arr[prev+1] = arr[prev]
+                prev--
+            
+            }
+            arr[prev+1] = cur
+            
+        }
+        
+    }
+}
