7dae0417c0bc6658b73bd87655e991c40ad639e3
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2025-09-25 12:36:44 +0530

    Create nextGreaterElement1.js

---
+++ ImportantQuestions/Monotonic Stack/nextGreaterElement1.js
@@ -0,0 +1,29 @@
+// brute force
+// TC -- O(n) * O(n2)
+
+var nextGreaterElement = function(nums1, nums2) {
+    const result = []
+    for(let i=0; i<nums1.length; i++) {
+        for(let j=0; j<nums2.length; j++) {
+            if(nums1[i] === nums2[j]) {
+                let found = false
+                for(let k = j+1; k<nums2.length; k++ ) {
+                    if(nums2[k] > nums2[j]) {
+                        found = true
+                        result[i] = nums2[k]
+                        break
+                    }
+                     
+
+                }
+                if(!found) {
+                    result[i] = -1
+                }
+
+            }
+        }
+
+    }
+    return result
+    
+};
